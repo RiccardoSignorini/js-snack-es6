4328e28d8696fc75336e3062f19a3770ff87beed
RiccardoSignorini <riccardo.signorini1@gmail.com>
2023-04-14 15:02:30 +0200

    apertura cartelle lavoro

---
+++ snack1/main.js
@@ -0,0 +1,8 @@
+/*
+---PROBLEMA---
+Creare un array di oggetti:
+
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+
+Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+*/
---
+++ snack3/main.js
@@ -0,0 +1,12 @@
+/*
+---PROBLEMA---
+Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
+
+L'array generato da parte vostra dovrà essere di, ad esempio, nomi o cose: ['alberto', 'michele','angelo','simone']
+
+Se come numeri ad esemio ho 2 e 4 devo inserire in un array nuovo l'elemento in posizione 3
+
+La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
+
+Usiamo i nuovi metodi degli array foreach o filter.
+*/
