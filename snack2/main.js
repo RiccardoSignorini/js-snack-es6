f8f40e04deca31cd3b429c45453d019ef63584ba
RiccardoSignorini <riccardo.signorini1@gmail.com>
2023-04-17 14:55:25 +0200

    creazione array e funzione

--- snack2/main.js
+++ snack2/main.js
@@ -5,8 +5,36 @@ Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse prop
 Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 
 Generare numeri random al posto degli 0 nelle proprietà:
-
 Punti fatti e falli subiti.
 
 Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 */
+
+// ARRAY DI OGGETTI
+const squadre = [
+    {
+        nomeSquadra: 'Manchester City',
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nomeSquadra: 'Roma',
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nomeSquadra: 'Barcellona',
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nomeSquadra: 'Bayern Monaco',
+        puntiFatti: 0,
+        falliSubiti: 0
+    }
+]
+
+// FUNZIONE NUMERI CASUALI
+function randomNum(){
+    return Math.floor(Math.random()*100)+1
+}
